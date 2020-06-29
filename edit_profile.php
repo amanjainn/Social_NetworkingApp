7f12ed0a92993ad70f7ba14456fc4039679e9ec4
Aman <amangrand@gmail.com>
2020-06-29 19:13:05 +0530

    Final commit

---
+++ edit_profile.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<?php
+session_start();
+include("includes/header.php");
+
+if(!isset($_SESSION['user_email'])){
+	header("location: index.php");
+}
+?>
+<html>
+<head>
+	<?php
+		$user = $_SESSION['user_email'];
+		$get_user = "select * from users where user_email='$user'";
+		$run_user = mysqli_query($con,$get_user);
+		$row = mysqli_fetch_array($run_user);
+
+		$user_name = $row['user_name'];
+	?>
+	<title><?php echo "$user_name"; ?></title>
+	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style/home_style2.css">
+</head>
+<body>
+<div class="row">
+    </div>
+</body>
+</html>
